a6c7f9a44b65b5c7c959e4f11afec20e829df275
Pascal <i13004@hb.dhbw-stuttgart.de>
2019-05-06 18:37:18 +0200

    Fixes for JUnit-Tests by Infomark
    - Fixed the calculations of the polynomial.

--- src/ex2/main/Imprecision.java
+++ src/ex2/main/Imprecision.java
@@ -27,14 +27,22 @@ public class Imprecision {
 	// Exercise Task
 	// 2.2 b)
 	public static double double_eval_polynom(double x0, double y0) {
+		double res = 0;
 
-		double res = 333.75d * Math.pow(y0, 6);
-		res += 11d * Math.pow(x0, 4) * Math.pow(y0, 2);
-		res -= Math.pow(y0, 6) * Math.pow(x0, 2);
-		res -= 121d * Math.pow(y0, 4) * Math.pow(x0, 2);
-		res -= 2d * Math.pow(x0, 2);
+		res += 333.75d * Math.pow(y0, 6);
+		res += Math.pow(x0, 2)
+				* (11d * Math.pow(y0, 2) * Math.pow(x0, 2) - Math.pow(y0, 6) - 121d * Math.pow(y0, 4) - 2d);
 		res += 5.5d * Math.pow(y0, 8);
 		res += x0 / (2d * y0);
+
+		// best value
+//		res += 333.75d * Math.pow(y0, 6);
+//		res += 11d * Math.pow(x0, 4) * Math.pow(y0, 2);
+//		res -= Math.pow(y0, 6) * Math.pow(x0, 2);
+//		res -= 121d * Math.pow(y0, 4) * Math.pow(x0, 2);
+//		res -= 2d * Math.pow(x0, 2);
+//		res += 5.5d * Math.pow(y0, 8);
+//		res += x0 / (2d * y0);
 
 		return res;
 	}
@@ -44,13 +52,20 @@ public class Imprecision {
 	public static float float_eval_polynom(float x0, float y0) {
 		float res = 0;
 
-		res -= (float) Math.pow(y0, 6) * (float) Math.pow(x0, 2);
-		res += 11f * (float) Math.pow(x0, 4) * (float) Math.pow(y0, 2);
 		res += 333.75f * (float) Math.pow(y0, 6);
-		res -= 121f * (float) Math.pow(y0, 4) * (float) Math.pow(x0, 2);
-		res -= 2f * (float) Math.pow(x0, 2);
+		res += (float) Math.pow(x0, 2) * (11f * (float) Math.pow(y0, 2) * (float) Math.pow(x0, 2)
+				- (float) Math.pow(y0, 6) - 121f * (float) Math.pow(y0, 4) - 2f);
 		res += 5.5f * (float) Math.pow(y0, 8);
 		res += x0 / (2f * y0);
+
+		// best value
+//		res -= (float) Math.pow(y0, 6) * (float) Math.pow(x0, 2);
+//		res += 11f * (float) Math.pow(x0, 4) * (float) Math.pow(y0, 2);
+//		res += 333.75f * (float) Math.pow(y0, 6);
+//		res -= 121f * (float) Math.pow(y0, 4) * (float) Math.pow(x0, 2);
+//		res -= 2f * (float) Math.pow(x0, 2);
+//		res += 5.5f * (float) Math.pow(y0, 8);
+//		res += x0 / (2f * y0);
 
 		return res;
 	}
@@ -59,7 +74,7 @@ public class Imprecision {
 		System.out.println("2.2 a) ");
 		System.out.println(different_same_sum(2.97f));
 		System.out.println("2.2 b) ");
-		System.out.println(double_eval_polynom(1, 1));
-		System.out.println(float_eval_polynom(1, 1));
+		System.out.println(double_eval_polynom(77617, 33096)); // 1, 1
+		System.out.println(float_eval_polynom(77617, 33096)); // 1, 1
 	}
 }
