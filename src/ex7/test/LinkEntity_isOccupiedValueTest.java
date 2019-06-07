cffe85218a72d3fd8c0c1c43585f9f054436716d
Pascal <i13004@hb.dhbw-stuttgart.de>
2019-06-07 21:57:24 +0200

    JUnit Test
    - added JUnitTest based on TestMessage on Infomark.

---
+++ src/ex7/test/LinkEntity_isOccupiedValueTest.java
@@ -0,0 +1,18 @@
+package ex7.test;
+
+import org.junit.jupiter.api.Test;
+
+import ex7.main.Snake;
+import junit.framework.TestCase;
+
+class LinkEntity_isOccupiedValueTest extends TestCase {
+
+	@Test
+	void test() {
+		Snake snake = new Snake(2, 1, 1, 1);
+		snake.setNext(new Snake(2, 2, 1, 1));
+		boolean b = snake.isOccupied(2, 1);
+		assertEquals(true, b);
+	}
+
+}
