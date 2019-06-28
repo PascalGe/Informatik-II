fa3bd19ff0e3aee4fa9e442fe0d55b3f1a37e612
Pascal <pascal.gepperth@student.uni-tuebingen.de>
2019-06-28 09:11:10 +0200

    Finished Exercise 2
    - Implemented methods.

--- src/ex9/main2/EmptyTree.java
+++ src/ex9/main2/EmptyTree.java
@@ -1,37 +1,43 @@
 package ex9.main2;
 
+/**
+ * 
+ * @author Pascal Gepperth (4005085)
+ *
+ */
 public class EmptyTree implements SortedFloatTree {
 
-    public EmptyTree() {
-    }
+	public EmptyTree() {
+	}
 
-    @Override
-    public String asString() {
-	return "";
-    }
+	@Override
+	public String asString() {
+		return "";
+	}
 
-    @Override
-    public int depth() {
-	// TODO
-	return 0; 
-    }
+	@Override
+	public int depth() {
+		return 0;
+	}
 
-    @Override
-    public int nodeCount() {
-	// TODO
-	return 0; 
-    }
+	@Override
+	public int nodeCount() {
+		return 0;
+	}
 
-    @Override
-    public boolean exists(float element) {
-	// TODO
-	return false;
-    }
+	@Override
+	public boolean exists(float element) {
+		return false;
+	}
 
-    @Override
-    public SortedFloatTree insert(float element) {
-	// TODO
-	return this;
-    }
+	@Override
+	public SortedFloatTree insert(float element) {
+		// TODO
+		return new Node(element, new EmptyTree(), new EmptyTree());
+	}
 
+	@Override
+	public SortedFloatTree getClone() {
+		return new EmptyTree();
+	}
 }
